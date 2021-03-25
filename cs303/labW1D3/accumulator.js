bc71540e9003b134c18572e271766717d996cee8
haben2022 <berhanetesfahannes11@gmail.com>
2021-03-25 00:46:10 -0500

    assignment W1D3

--- cs303/labW1D3/accumulator.js
+++ cs303/labW1D3/accumulator.js
@@ -5,7 +5,18 @@
 
  * @returns {Accumulator} constructor function
  */
-function Accumulator(){
+function Accumulator(firstValue){
 //implement this
+this.value = firstValue;
+  
+this.text = function() {
+  this.value += parseInt(prompt('How much to add?'));
+};
 
 }
+
+let accumulator = new Accumulator(1);
+accumulator.text();
+accumulator.text();
+console.log(accumulator.value);
+
--- cs303/labW1D3/calc.js
+++ cs303/labW1D3/calc.js
@@ -1,8 +1,28 @@
-"use strict"
+"use strict";
 
 /**
  * @returns {Calculator} this is a constructor function
  */
 function Calculator() {
- //implement this
-  }
+    this.text = function() {
+        this.num1 = parseInt(prompt("enter first number:"));
+        this.num2 = parseInt(prompt("enter second number:"));
+      };
+    
+      this.sum = function() {
+        return this.num1 + this.num2;
+      };
+    
+      this.multp = function() {
+        return this.num1 * this.num2;
+      };
+    }
+    
+    let calculator = new Calculator();
+    calculator.text();
+    
+   console.log( "Sum=" + calculator.sum() );
+   
+        
+    console.log( "Mul=" + calculator.multp() );
+  
